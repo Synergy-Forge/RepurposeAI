60a3ef34bbf64019e83745c0f4ef1f27c6bb9fb7
Felipe Abe <abe.fydr@gmail.com>
2025-09-07 02:41:19 -0300

    fix: next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  output: process.env.NODE_ENV === 'production' ? "standalone" : undefined,
+  output: process.env.CI === 'true' ? "standalone" : undefined,
 };
 
 export default nextConfig;
